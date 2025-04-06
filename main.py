a8d6edce05f5bb0a657cbd41d5aa02dcadf14756
Daniel Zhelenzov <Daniel.Zheleznov@gmail.com>
2025-04-06 13:11:54 -0400

    Now proper rendering of the pixels is a thing.   EDIT: Had to remove my test level from main.

--- main.py
+++ main.py
@@ -4,7 +4,4 @@ import custom_states as states
 if __name__ == '__main__':
     game = green.Game()
 
-    fantasia_state = states.fantasia(game, None)
-    game.add_state(fantasia_state)
-
     game.run()
